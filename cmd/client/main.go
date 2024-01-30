5c327162c18e3b074760c76f489d88d2bf11530b
Nikolay Kozlov <gremiha3@gmail.com>
2024-01-30 20:51:43 +0300

    добавил комментарии

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -13,38 +13,47 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-var (
+var ( //входные аргументы программы
 	addr = flag.String("addr", "localhost:50051", "the address to connect to")
 )
 
 func main() {
-	flag.Parse()
-	if flag.NArg() < 2 {
-		log.Fatal("not enough arguments")
+	flag.Parse()         //опрашиваем входные аргументы программы
+	if flag.NArg() < 2 { //если аргументов недостаточно, то
+		log.Fatal("not enough arguments") //пишем в консоль ошибку и останавливаем программу
 	}
 
-	x, err := strconv.Atoi(flag.Arg(0))
-	if err != nil {
-		log.Fatal(err)
+	x, err := strconv.Atoi(flag.Arg(0)) //считываем первый аргумент и приводим его к типу INT
+	if err != nil {                     //если привести к типу INT нельзя, то
+		log.Fatal(err) //пишем в консоль ошибку и останавливаем программу
 	}
 	y, err := strconv.Atoi(flag.Arg(1))
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err) //пишем в консоль ошибку и останавливаем программу
 	}
+	//создаем клиентское подключение к адресу с портом, используя при этом небезопасный метод
 	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+	if err != nil { //если подключение не удалось, то
+		log.Fatalf("did not connect: %v", err) //пишем в консоль ошибку и останавливаем программу
 	}
-	defer conn.Close()
-	c := pb.NewAdderClient(conn)
+	defer conn.Close()           //По достижению конца функции main, подключение закроется автоматически
+	c := pb.NewAdderClient(conn) //создаем объект AdderClient, подключенный через созданное соединение
 
+	//создаем контекст с таймаутом в 1 сек на основе пустого контекста.
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
+	defer cancel() //если код ниже до конца программы не успеет завершиться за 1 секунду, то
+	//контекст завершит программу принудительно, иначе
+	//выполнится код ниже, и по окончании программы, сработает "defer cancel()" и отменит контекст
 
+	//вызывая функцию Add объекта AdderClient, передаем :
+	//
+	//            созданный|      структура передаваемых
+	//            контекст |      данных серверу
 	res, err := c.Add(ctx, &pb.AddRequest{X: int32(x), Y: int32(y)})
-	if err != nil {
-		log.Fatalf("could not greet: %v", err)
+	// и возвращаем результат обработки данных от сервера
+	if err != nil { //если получили ошибку функции сложения, то
+		log.Fatalf("could not greet: %v", err) //пишем в консоль ошибку и останавливаем программу
 	}
-	log.Println(res.GetResult())
+	log.Println(res.GetResult()) //иначе, печатаем результат
 
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,34 +7,44 @@ import (
 	"log"
 	"net"
 
-	pb "gogrpcvuejs/pkg/api"
+	pb "gogrpcvuejs/pkg/api" //алиас на папку со сгенерированным protobuf файлом
 
 	"google.golang.org/grpc"
 )
 
-var (
+var ( //опрашиваем параметры командной строки
+	// -port 8080
+	// если ничего не указывать, то по умолчанию будет 50051
+	// если запустить с параметром -h/--help , то выведет справку
 	port = flag.Int("port", 50051, "The server port")
 )
 
-// server is used to implement helloworld.GreeterServer.
+// структура server используется для реализации интерфейса pb.AdderServer.
 type server struct {
 	pb.UnimplementedAdderServer
 }
 
-// Add implements Add.AdderServer
+// Добавляем реализацию интерфейса pb.AdderServer через метод Add структуры server
+//
+//	type AdderServer interface {
+//		Add(context.Context, *AddRequest) (*AddResponse, error)
+//		mustEmbedUnimplementedAdderServer()
+//	}
+//
+//	структура         контекст			  входное сообщение	  выходное сообщение
 func (s *server) Add(ctx context.Context, in *pb.AddRequest) (*pb.AddResponse, error) {
 	log.Printf("Received: %v, %v", in.GetX(), in.GetY())
 	return &pb.AddResponse{Result: in.GetX() + in.GetY()}, nil
 }
 
 func main() {
-	flag.Parse()
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+	flag.Parse()                                             //обрабатываем входные аргументы программы
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port)) //запускаем прослушивание сети
+	if err != nil {                                          //если запуск сети произошел с ошибкой, то
+		log.Fatalf("failed to listen: %v", err) //выводим лог в терминал и закрываем программу
 	}
-	s := grpc.NewServer()
-	pb.RegisterAdderServer(s, &server{})
+	s := grpc.NewServer()                //создаем gRPC-сервер, который еще не имеет зарегистрированных служб и не начал принимать запросы.
+	pb.RegisterAdderServer(s, &server{}) //регистрируем службу AdderServer на сервере "s" с указанием адреса структуры server
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
